14b8695c88e102709b4f34b0fb43eeb5292462d0
TechyByte <info@techybyte.co.uk>
2024-05-14 17:39:03 +0100

    ML improvements

--- learn.py
+++ learn.py
@@ -12,10 +12,12 @@ import numpy as np
 # from tensorflow import keras
 
 from keras.models import Sequential
-from keras.layers import Conv1D, LSTM, Dense
-from keras.layers import Dropout, BatchNormalization, Reshape
-from sklearn.preprocessing import MinMaxScaler
+from keras.layers import Conv1D, LSTM, Dense, Dropout, Reshape, Normalization, BatchNormalization
+from keras.callbacks import ReduceLROnPlateau
+from sklearn.preprocessing import MinMaxScaler, KBinsDiscretizer, LabelBinarizer
 from sklearn.model_selection import train_test_split
+
+learning_rate_reduction = ReduceLROnPlateau(monitor='val_loss', patience=2, verbose=1, factor=0.75, min_lr=0.00001)
 
 logging.info(f"Loading historical model: {config.historical_model_filename}")
 with open(config.historical_model_filename, "rb") as file:
@@ -55,7 +57,8 @@ for node, data in raw_model.nodes(data=True):
     if count % int(total / 100) == 0:
         toc = time.perf_counter()
         logging.info(
-            f"Processing nodes: {100 * count / total:0.4f}% ({count} of {total}) - estimated time remaining: {(toc - tic) * ((total - count) / count) / 60:0.4f} minutes")
+            f"Processing nodes: {100 * count / total:0.4f}% ({count} of {total}) - {success} successful so far - "
+            f" - estimated time remaining: {(toc - tic) * ((total - count) / count) / 60:0.4f} minutes")
 
     # If a specific node is provided and the current node is not the specified node, skip this iteration
     if node_to_learn is not None and node != node_to_learn:
@@ -84,13 +87,15 @@ for node, data in raw_model.nodes(data=True):
                                                            incident[['TRAIN_SERVICE_CODE']]
                                                           .values, closest_weather.values.flatten()])
 
-                combined_plus_created_features = np.concatenate([[str(node), incident_datetime_int64 + 10],
-                                                                 [str(int(incident['TRAIN_SERVICE_CODE']) + 10)],
-                                                                 closest_weather.values.flatten()])
+                # Create a new "later" feature with the 0 delay
+                #combined_plus_created_features = np.concatenate([[str(node), incident_datetime_int64 + 10],
+                #                                                 [str(int(incident['TRAIN_SERVICE_CODE'])/2)],
+                #                                                 closest_weather.values.flatten()])
 
-                combined_minus_created_features = np.concatenate([[str(node), incident_datetime_int64 - 10],
-                                                                 [str(int(incident['TRAIN_SERVICE_CODE']) - 10)],
-                                                                 closest_weather.values.flatten()])
+                # Create a new "earlier" feature with the 0 delay
+                #combined_minus_created_features = np.concatenate([[str(node), incident_datetime_int64 - 10],
+                #                                                 [str(int(incident['TRAIN_SERVICE_CODE'])/2)],
+                #                                                 closest_weather.values.flatten()])
                 # Add the combined features to the list
                 features.append(combined_features_actual)
 
@@ -99,10 +104,10 @@ for node, data in raw_model.nodes(data=True):
                 # incident['INCIDENT_REASON']
 
                 labels.append(label)
-                features.append(combined_plus_created_features)
-                features.append(combined_minus_created_features)
-                labels.append(0)
-                labels.append(0)
+                #features.append(combined_plus_created_features)
+                #features.append(combined_minus_created_features)
+                #labels.append(0)
+                #labels.append(0)
                 success += 1
             else:
                 logging.debug(f"No weather history data available for node {node}")
@@ -113,8 +118,8 @@ labels = np.array(labels)
 
 logging.info("Features and labels extracted")
 
-from sklearn.preprocessing import LabelEncoder
 
+from sklearn.preprocessing import LabelEncoder
 # Initialize the LabelEncoder
 le = LabelEncoder()
 
@@ -141,25 +146,29 @@ logging.info("Complete")
 # le.fit(np.array([column for column in y_combined.T]))
 # y_train = np.array([le.transform(column) for column in y_train.T]).T
 # y_val = np.array([le.transform(column) for column in y_val.T]).T
+
 logging.info("Preparing to train...")
 # Define the model
 model = Sequential()
-model.add(Dense(units=128, kernel_initializer='he_uniform', activation='relu', input_shape=(X_train.shape[1],)))
-model.add(Reshape((128, 1)))
-model.add(Conv1D(filters=128, kernel_size=9,
-                 activation='sigmoid'))  # , input_shape=(X_train.shape[1], 1)))  # Increased number of filters
-model.add(Dropout(0.1))
+model.add(BatchNormalization())
+# model.add(Normalization(axis=1, input_shape=(X_train.shape[1],)))
+model.add(Dense(units=32, kernel_initializer='he_uniform', activation='relu', input_shape=(X_train.shape[1],)))
+# model.add(Reshape((32, 1)))
+model.add(LSTM(units=32, return_sequences=True, dropout=0, use_bias=True,
+               activation='tanh', recurrent_activation='sigmoid', recurrent_dropout=0, unroll=False))
+model.add(Conv1D(filters=32, kernel_size=9, activation='sigmoid'))
 
 # model.add(Conv1D(filters=64, kernel_size=3, activation='relu'))
-model.add(LSTM(units=96, return_sequences=True,
-               activation='tanh'))
+model.add(Dropout(0.3))
+# model.add(LSTM(units=32, return_sequences=False))
+# model.add(Dense(units=32, activation='softmax'))
+model.add(Dense(units=1, activation='linear'))
 
-# model.add(LSTM(units=50, return_sequences=False))
-model.add(Dense(units=32, activation='softmax'))
-model.add(Dense(units=1))
+from keras.optimizers import Adam, SGD, Adagrad
 
 # Compile the model
-model.compile(optimizer='adam', loss='mean_squared_logarithmic_error')
+model.compile(optimizer=SGD(learning_rate=0.0001), loss='mean_squared_logarithmic_error',
+              metrics=['mse', 'mae'])
 
 # Reshape the data to fit the model
 X_train = X_train.reshape((X_train.shape[0], X_train.shape[1], 1))
@@ -167,7 +176,10 @@ X_val = X_val.reshape((X_val.shape[0], X_val.shape[1], 1))
 
 logging.info("Training model...")
 # Train the model
-history = model.fit(X_train, y_train, validation_data=(X_val, y_val), epochs=8, batch_size=64)
+history = model.fit(X_train, y_train,
+                    validation_data=(X_val, y_val),
+                    epochs=6, batch_size=48,
+                    callbacks=[learning_rate_reduction])
 logging.info("Training complete")
 
 # Save the model
@@ -205,10 +217,20 @@ print(predictions)
 
 from sklearn.metrics import confusion_matrix
 
+y_val_continuous = y_val
+
 # Round the predictions to the nearest integer
 rounded_predictions = np.round(predictions)
 
-# Generate the confusion matrix
-matrix = confusion_matrix(y_val, rounded_predictions)
+# Use KBinsDiscretizer to discretize continuous values into bins
+discretizer = KBinsDiscretizer(n_bins=1000, encode='ordinal', strategy='uniform')
+y_val_discrete = discretizer.fit_transform(y_val_continuous.reshape(-1, 1))
 
-print(matrix)
+y_val_multiclass = y_val_discrete
+
+# Use LabelBinarizer to convert multiclass labels to binary labels
+binarizer = LabelBinarizer()
+y_val_binary = binarizer.fit_transform(y_val_multiclass)
+
+# Now y_val_binary can be used with confusion_matrix
+matrix = confusion_matrix(y_val_binary, rounded_predictions)
